2d69ec3b06eeab57d19e2176a5a4ca2974805fa7
ZachSaeed <syed.saeed@tillertech.com>
2021-08-25 21:15:36 +0100

    Added all files

---
+++ 075 Modules/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,5 @@
+from my_code_files.my_class_file import Saquib
+from my_code_files.my_class_file import Saquib
+
+my_obj = Saquib()
+my_obj.print_my_name()
---
+++ 075 Modules/my_code_file.py
@@ -0,0 +1,12 @@
+def fn():
+    return "do some stuff"
+
+
+def another_fn():
+    return "do some other stuff"
+
+
+class Clss:
+    
+    def mthd(self):
+        return "Method from Clss called"
